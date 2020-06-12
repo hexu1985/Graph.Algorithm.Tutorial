acc36d79179e07c7f167ac21d8321c75bd721840
hexu <hexu@sensetime.com>
2020-06-12 11:23:26 +0800

    update chapter-01/recipe-04

---
+++ chapter-01/recipe-04/src/sample_weight_dense_digraph2.cpp
@@ -0,0 +1,65 @@
+/** \example sample_weight_dense_digraph2.cpp
+ * This is an example of how to use the weight::dense_graph class.
+ */
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "weight_dense_graph.hpp"
+#include "weight_dense_graph_dot.hpp"
+
+using namespace std;
+using namespace weight;
+
+using Edge = dense_graph::edge_type;
+using Graph = dense_graph;
+
+int main(int argc, char *argv[])
+{
+    string dot_file = "dense_digraph.dot";
+    if (argc >= 2) {
+        dot_file = argv[1];
+    }
+
+    int vertexNumber = 6;
+    vector<Edge> edges = {
+        {0,1, .41},
+        {1,2, .51},
+        {2,3, .50},
+        {4,3, .36},
+        {3,5, .38},
+        {3,0, .45},
+        {0,5, .29},
+        {5,4, .21},
+        {1,4, .32},
+        {4,2, .32},
+        {5,1, .29}
+    };
+
+    auto graph = Graph::make_digraph(vertexNumber);
+
+    cout << "insert edges\n";
+    for (auto &edge: edges)
+        graph->insert(&edge);
+
+    // show edges
+    cout << graph->edge_count() << " edges in graph" << endl;
+
+    cout << "graph to dot format:\n"
+        << to_dot(*graph)
+        << endl;
+
+    // save dot file
+    cout << "save graph to " << dot_file << " ...\n";
+    ofstream ofile(dot_file);
+    if (!ofile) {
+        cout << "open " << dot_file << " failed!\n";
+        exit(1);
+    }
+
+    write_dot(ofile, *graph);
+
+    cout << "write dot file success!" << endl;
+
+    return 0;
+}
--- chapter-01/recipe-04/src/test_sample.sh
+++ chapter-01/recipe-04/src/test_sample.sh
@@ -11,15 +11,15 @@ echo "./sample_weight_dense_graph2 weight_dense_graph2.dot"
 dot weight_dense_graph2.dot -T png -o weight_dense_graph2.png
 echo "output png is weight_dense_graph2.png"
 
-#echo
-#
-#echo "./sample_weight_dense_digraph1"
-#./sample_weight_dense_digraph1
-#
-#echo
-#echo
-#
-#echo "./sample_weight_dense_digraph2 weight_dense_digraph2.dot"
-#./sample_weight_dense_digraph2 weight_dense_digraph2.dot
-#dot weight_dense_digraph2.dot -T png -o weight_dense_digraph2.png
-#echo "output png is weight_dense_digraph2.png"
+echo
+
+echo "./sample_weight_dense_digraph1"
+./sample_weight_dense_digraph1
+
+echo
+echo
+
+echo "./sample_weight_dense_digraph2 weight_dense_digraph2.dot"
+./sample_weight_dense_digraph2 weight_dense_digraph2.dot
+dot weight_dense_digraph2.dot -T png -o weight_dense_digraph2.png
+echo "output png is weight_dense_digraph2.png"
