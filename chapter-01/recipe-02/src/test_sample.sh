fc2d929d82e6c9f6e040fb60559e50dc1e5be1f2
hexu <hexu@sensetime.com>
2020-08-10 17:15:52 +0800

    add sample_unweight_dense_graph_dot1.cpp

--- chapter-01/recipe-02/src/test_sample.sh
+++ chapter-01/recipe-02/src/test_sample.sh
@@ -11,6 +11,12 @@ echo "./sample_unweight_dense_graph2 unweight_dense_graph2.dot"
 dot unweight_dense_graph2.dot -T png -o unweight_dense_graph2.png
 echo "output png is unweight_dense_graph2.png"
 
+echo "./sample_unweight_dense_graph_dot1 unweight_dense_graph_dot1.dot"
+./sample_unweight_dense_graph_dot1 unweight_dense_graph_dot1.dot 
+dot unweight_dense_graph_dot1.dot -T png -o unweight_dense_graph_dot1.png
+echo "output png is unweight_dense_graph_dot1.png"
+
+echo
 echo
 
 echo "./sample_unweight_dense_digraph1"
